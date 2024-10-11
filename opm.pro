0a7cfb9a43fa1faa8fdf7380bf66f63b056f2b7e
Odizinne <flora.aubry@proton.me>
2024-10-11 11:42:21 +0200

    restructuration

--- opm.pro
+++ opm.pro
@@ -7,7 +7,8 @@ CONFIG += c++17 cmdline
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-        main.cpp
+        main.cpp \
+        packagemanager.cpp
 
 LIBS += -lShell32 -lole32
 
@@ -15,3 +16,6 @@ LIBS += -lShell32 -lole32
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+HEADERS += \
+    packagemanager.h
---
+++ packagemanager.h
@@ -0,0 +1,41 @@
+#ifndef PACKAGEMANAGER_H
+#define PACKAGEMANAGER_H
+
+#include <QString>
+#include <QJsonArray>
+
+class PackageManager
+{
+public:
+    PackageManager();
+    void help();
+    void selfinstall();
+    void update();
+    void list();
+    bool copyRecursively(const QString &sourcePath, const QString &destinationPath);
+    QString greenText(const QString &text);
+    bool checkAndKillProcess(const QString &executable);
+    void restartProcess(const QString &executable);
+    void install(const QStringList &packageNames);
+    void remove(const QStringList &packageNames);
+    void upgrade();
+
+private:
+    QString manifestUrl = "https://raw.githubusercontent.com/Odizinne/opm-manifest/refs/heads/main/manifest.json";
+    QString appDataDir;
+    QString manifestFile;
+    QString installedPackagesFile;
+    QJsonArray manifest;
+    QMap<QString, QString> installedVersions;
+    void fetchManifest();
+    void loadManifest();
+    void parseInstalledPackages();
+    void downloadPackage(const QString &url, const QString &packageName, const QString &version);
+    void extractZip(const QString &zipFilePath, const QString &destDir, const QString &packageName);
+    void saveInstalledPackages();
+    void createStartMenuEntry(const QString &projectName);
+
+
+};
+
+#endif // PACKAGEMANAGER_H
